60387d789ef1888e184914272550e007e4e68ce3
JohnV <velizjuanagustin@gmail.com>
2020-11-17 16:38:04 -0300

    Removed request limit

--- SEACBE/Program.cs
+++ SEACBE/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace SEACBE
 {
@@ -20,7 +14,10 @@ namespace SEACBE
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseKestrel(options => {
+                        options.Limits.MaxRequestBodySize = null;
+                    });
                 });
     }
 }
